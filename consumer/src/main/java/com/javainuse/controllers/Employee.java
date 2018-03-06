ff7d4cc1903b100a050588f0eb2d81312dd1fc24
Admin <Admin@NODE812>
2018-03-06 11:59:07 +0530

    first commit for Employee object

---
+++ consumer/src/main/java/com/javainuse/controllers/Employee.java
@@ -0,0 +1,45 @@
+package com.javainuse.controllers;
+
+public class Employee {
+
+	private Long empId;
+	private String name;
+	private String designation;
+	private double salary;
+
+	public Employee() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public Long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+
+}
